d98a48ac9c4fb4948c9447e2a75bfe76c565587e
mbpmd2020 <mbp2020@MacBook-Pro.local>
2021-12-01 13:12:39 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.vector import Vector
+from random import randint
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        vx, vy = ball.velocity
+        offset = (ball.center_y - self.center_y) / (self.height / 2)
+        bounced = Vector(-1 * vx, vy)
+        vel = bounced * 1.1
+        ball.velocity = vel.x, vel.y + offset
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        if(self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+        if(self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.player2.center_y = touch.y
+
+
+
+
+if __name__ == "__main__":
+    PongApp().run()
